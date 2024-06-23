9b1c4fcb1c65778a03d43fbf354af47143608d94
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-06-23 20:05:26 +0100

    wip

--- api.php
+++ api.php
@@ -1,5 +1,18 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-echo json_encode(["message" => "Hello Mark"]);
+
+// Gather client API details
+$client_ip = $_SERVER['REMOTE_ADDR'];
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+// Prepare the response
+$response = [
+    "message" => "Hello Mark",
+    "client_ip" => $client_ip,
+    "user_agent" => $user_agent
+];
+
+// Send the JSON response
+echo json_encode($response);
 ?>
