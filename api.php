221a9608096c73a7dda8a592220170b4ca32a0ae
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-06-27 12:35:47 +0100

    fix: greeting

--- api.php
+++ api.php
@@ -1,18 +1,29 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-// Gather client API details
-$client_ip = $_SERVER['REMOTE_ADDR'];
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
+// Get the visitor's name from the query parameter
+$visitor_name = isset($_GET['visitor_name']) ? htmlspecialchars($_GET['visitor_name']) : 'Guest';
 
-// Prepare the response
+// Get the client's IP address
+$client_ip = $_SERVER['REMOTE_ADDR'];
+
+
+// Get location data from ip-api.com
+$location = "Unknown location"; // Default location
+$api_url = "http://ip-api.com/json/{$client_ip}";
+$location_data = file_get_contents($api_url);
+if ($location_data) {
+    $location_data = json_decode($location_data, true);
+    if ($location_data['status'] === 'success') {
+        $location = $location_data['city'] . ', ' . $location_data['regionName'] . ', ' . $location_data['country'];
+    }
+}
+
 $response = [
-    "message" => "Hello Mark",
-    "client_ip" => $client_ip,
-    "user_agent" => $user_agent
+    'client_ip' => $client_ip,
+    'location'=> $location,
+    'greeting' => "Hello, $visitor_name!"
 ];
 
-// Send the JSON response
 echo json_encode($response);
 ?>
